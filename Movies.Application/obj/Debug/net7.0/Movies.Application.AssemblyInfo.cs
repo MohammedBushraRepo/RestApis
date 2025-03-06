0568b35c8b16fa1f659145b6beb7f4b46fa161c8
mohamed <mohammad.bushra@mensagroup.net>
2025-03-06 14:55:09 +0400

    finalize gitignore problem

--- Movies.Application/obj/Debug/net7.0/Movies.Application.AssemblyInfo.cs
+++ Movies.Application/obj/Debug/net7.0/Movies.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7315adb85d7d1c18931508540fcf74a534b720c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aca7ddb984cef12b8a90b4f9bb791ee2fedf880")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
